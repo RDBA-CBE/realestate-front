62a487d35680648747c67c0b1a1f39ff14fc0f2a
Duraisamy303 <psmkduraisamy@gmail.com>
2025-09-22 11:05:01 +0530

    esline updated

--- components/real-estate/property-detail/Nearby.component.tsx
+++ components/real-estate/property-detail/Nearby.component.tsx
@@ -113,7 +113,7 @@ export default function Nearby() {
 
   return (
     <Card className="rounded-2xl shadow p-6">
-      <h3 className="text-lg font-semibold mb-4">What's Nearby?</h3>
+     <h3 className="text-lg font-semibold mb-4">{"What's Nearby?"}</h3>
 
 
       <CardContent>
