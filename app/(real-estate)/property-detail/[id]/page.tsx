18e23d20153e6aed8afb572a3a9d760b25642dd5
Pragathi-29 <pragathisarathi02@gmail.com>
2025-10-16 12:19:16 +0530

    Margin top removed for property detail page

--- app/(real-estate)/property-detail/[id]/page.tsx
+++ app/(real-estate)/property-detail/[id]/page.tsx
@@ -290,7 +290,7 @@ export default function PropertyDetailPage() {
 
       <PropertyTabs />
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-10">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2 space-y-3">
           <div id="overview">
             <PropertyDetails data={state.detail} />
