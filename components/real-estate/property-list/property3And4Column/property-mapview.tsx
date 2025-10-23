2587fdd2cb5a6f35ac59aa698f62b16bcd1e2481
Pragathi-29 <pragathisarathi02@gmail.com>
2025-10-23 17:10:45 +0530

    Map view

--- components/real-estate/property-list/property3And4Column/property-mapview.tsx
+++ components/real-estate/property-list/property3And4Column/property-mapview.tsx
@@ -134,13 +134,8 @@ export function MapView(props) {
         <div className=" xl:col-span-5 p-6 lg:p-8 overflow-y-auto h-[calc(100vh-98px)] flex flex-col items-start">
           {/* First sticky header */}
           <div
-<<<<<<< HEAD
             className='sticky top-0 lg:-top-8 z-10 w-full p-5 rounded-lg mb-7'
             style={{ backgroundColor: '#f3f4f6' }}
-=======
-            className="sticky top-0 z-10 w-full p-5 rounded-lg mb-7"
-            style={{ backgroundColor: "#f3f4f6" }}
->>>>>>> 741782131d6e1144e767958d3486d53f64f30d7a
           >
             {/* First sticky header */}
             <div className="flex items-center justify-between mb-6 w-full">
