5b8784f82b8a7f52747db3f9fce4381efa975e11
Duraisamy303 <psmkduraisamy@gmail.com>
2026-06-06 12:40:29 +0530

    Ai search testing file added

---
+++ app/(real-estate)/testing/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AISearchComponent from "@/components/common-components/AiSearchTest";
+
+export default function AISearch() {
+  return (
+    <div className="w-full">
+      <AISearchComponent />
+    </div>
+  );
+}
